0c397a679bb4d55d5e50239dccbc4ef5eb534c33
pedronxaa <118825522+pedronxaa@users.noreply.github.com>
2023-06-07 22:16:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from pre_processador import pre_processar
 from sys import argv
 
-#caminho_h = input('Digite o caminho da pasta das bibliotecas padrão: ')
-#caminho_arquivo = input('Digite o caminho do arquivo em c: ')
-print(argv[0])
-pre_processar(argv[1],'')
+pre_processar(argv[1],argv[2])
